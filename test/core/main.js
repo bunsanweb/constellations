8681a8e0c397b0054fed3bf4ff175272e5c312c4
bellbind <bellbind@gmail.com>
2020-05-29 13:38:28 +0900

    [update]  text/core as automated test

--- test/core/main.js
+++ test/core/main.js
@@ -1,5 +1,6 @@
 // NOTE: not test sytle example
-import * as Core from "./modules/core.js";
+//import * as Core from "./modules/core.js";
+import * as Core from "http://localhost:10000/core.js";
 
 const core = new Core.Core();
 //console.log(core);
@@ -34,6 +35,13 @@ const stardusts2 = [
    names: ["foo", "bar", "buzz"]},
 ];
 
+const sameSet = (a, b) => {
+  const s1 = new Set(a), s2 = new Set(b);
+  if (s1.size !== s2.size) return false;
+  for (const e of s1) if (!s2.has(e)) return false;
+  return true;
+};
+
 (async () => {
   //[publish 0]
   {
@@ -49,15 +57,19 @@ const stardusts2 = [
     const stream = core.subscribe({}, stardusts[0].url);
     // subscribe all from stardust[0].url
     //console.log(stream[Symbol.asyncIterator]); // Maybe not yet supported
-    
+
     const reader = stream.getReader();
     for (let i = 0; i < 4; i++) {
       const {value, done} = await reader.read();
       if (done) break;
       const {url, point, names} = value;
-      console.log("[subscribe1]", url); // stardust1,2,3
+      //console.log("[subscribe1]", url); // stardust1,2,3,4
+      console.assert(url === stardusts[i].url, `url ${i}`);
+      console.assert(point === stardusts[i].point, `point ${i}`);
+      console.assert(sameSet(names, stardusts[i].names), `names ${i}`);
     }
     reader.releaseLock();
+    await stream.cancel();
   }
   
   //[subscribe 2: receive with specified names from now]
@@ -65,15 +77,20 @@ const stardusts2 = [
     // WHATWG ReadableStream
     const stream = core.subscribe({names: ["foo"]});
     //console.log(stream[Symbol.asyncIterator]); // Maybe not yet supported
-    
+
+    const expects = stardusts2.filter(s => s.names.includes("foo"));
     const reader = stream.getReader();
     for (let i = 0; i < 3; i++) {
       const {value, done} = await reader.read();
       if (done) break;
       const {url, point, names} = value;
-      console.log("[subscribe2]", url); // stardust5,6,8
+      //console.log("[subscribe2]", url); // stardust5,6,8
+      console.assert(url === expects[i].url, `url ${i}`);
+      console.assert(point === expects[i].point, `point ${i}`);
+      console.assert(sameSet(names, expects[i].names), `names ${i}`);
     }
     reader.releaseLock();
+    await stream.cancel();
   })();
 
   //[publish 1]
@@ -89,15 +106,22 @@ const stardusts2 = [
     const stream = core.subscribe(
       {point: stardusts[0].point}, stardusts[0].url);
     
+    const expects = [...stardusts, ...stardusts2].filter(
+      s => s.point === stardusts[0].point);
     const reader = stream.getReader();
     for (let i = 0; i < 4; i++) {
       const {value, done} = await reader.read();
       if (done) break;
       const {url, point, names} = value;
-      console.log("[subscribe2]", url); // stardust1,2,5,6
+      //console.log("[subscribe2]", url); // stardust1,2,5,6
+      console.assert(url === expects[i].url, `url ${i}`);
+      console.assert(point === expects[i].point, `point ${i}`);
+      console.assert(sameSet(names, expects[i].names), `names ${i}`);
     }
     reader.releaseLock();
+    await stream.cancel();
   }
-  
+  //console.info("[finished]");
+  if (typeof window.finish === "function") window.finish();
 })().catch(console.error);
 
